45653739badbb38e6c51c6543783bac3a2efab4d
Dmitrii Gusev <amidg4x4dima@gmail.com>
2023-09-17 19:53:04 -0700

    wip

---
+++ src/builder.py
@@ -0,0 +1,15 @@
+import threading
+import os
+import argparse
+import sqlite3
+from gusevtech.cvgpt.content import Project, Job, 
+
+class Resume():
+    def __init__(self):
+
+
+class Portfolio():
+    def __init__(self):
+
+
+    def 
--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
 import os
-from gusevtech.cvgpt import Builder 
+import argparse
+from gusevtech.cvgpt.builder import Resume, Portfolio
 
 # main program
 def main():
-    cvbuilder = Builder()
+    # parse args before doing anything else
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--resume", help="let the software work on your resume")
+    parser.add_argument("--portfolio", help="work your portfolio database")
+    args = parser.parse_args()
+
+    # if we work on the resume
+    if args.resume:
+        print("work in progress")
+    # if we work on the portfolio database
+    if args.portfolio:
+        portfolio = Portfolio()
 
 if __name__ == "__main__":
     main()
